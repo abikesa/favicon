ebdaeb6910586426c1afc55fbe871a64b52686c5
abikesa <abikesa.sh@gmail.com>
2025-05-16 05:58:14 -0400

    favicons on fire

--- assets/favicon.py
+++ assets/favicon.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 # Load your base image (preferably PNG, square, with transparency)
-img = Image.open("favicon-dark.png")
+img = Image.open("../../logos/assets/ukubona-light.png")
 
 # Save as .ico with multiple sizes for best browser support
-img.save("favicon-dark.ico", format="ICO", sizes=[(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)])
+img.save("favicon-light.ico", format="ICO", sizes=[(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)])
